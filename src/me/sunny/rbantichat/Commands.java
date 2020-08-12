98a6b7c8405a943f6b542225cc7d64bf9f8b6665
HiImSunny <48017917+HiImSunny@users.noreply.github.com>
2020-08-13 03:38:00 +0700

    fix permissions and count time reload load

--- src/me/sunny/rbantichat/Commands.java
+++ src/me/sunny/rbantichat/Commands.java
@@ -3,9 +3,11 @@ package me.sunny.rbantichat;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
+import org.bukkit.entity.Player;
 
 public class Commands implements TabExecutor {
 
@@ -15,7 +17,7 @@ public class Commands implements TabExecutor {
 	public boolean onCommand(CommandSender cs, Command cmd, String label, String[] args) {
 			if (args.length == 0) {
 				cs.sendMessage("");
-				cs.sendMessage(("&cRBAntiChat &fversion &e1.0").replace("&", "§"));
+				cs.sendMessage(("&cRBAntiChat &fversion &e1.1").replace("&", "§"));
 				cs.sendMessage(("&7&oFrom Sunny With Love").replace("&", "§"));
 				cs.sendMessage("");
 				return true;
@@ -24,6 +26,11 @@ public class Commands implements TabExecutor {
 				if (cs.hasPermission("rbantichat.admin")) {
 					Main.getPlugin().reloadConfig();
 					cs.sendMessage(("&8[&cRBAntiChat&8] &aReload lại config thành công!").replace("&", "§"));
+					for (Player a : Bukkit.getOnlinePlayers()) {
+						if (a.hasPermission("rbantichat.admin")) {
+							a.sendMessage(("&8[&cRBAntiChat&8] &aReload lại config thành công &f&o[&e&o" + Main.getlastStoppedTime() + "s&f&o]").replace("&", "§"));
+						}
+					}
 					return true;
 				}
 				cs.sendMessage(("&8[&cRBAntiChat&8] &aBạn không có quyền sử dụng lệnh này!").replace("&", "§"));
--- src/me/sunny/rbantichat/Main.java
+++ src/me/sunny/rbantichat/Main.java
@@ -1,25 +1,26 @@
 package me.sunny.rbantichat;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 
-	private long currentTime = 0L;
-	private double lastStoppedTime = 0.0D;
+	public static long currentTime = 0L;
+	public static double lastStoppedTime = 0.0D;
 
-	public void start() {
-		this.currentTime = System.currentTimeMillis();
+	public static void start() {
+		currentTime = System.currentTimeMillis();
 	}
 
-	public void stop() {
+	public static void stop() {
 		lastStoppedTime = (System.currentTimeMillis() - currentTime) / 1000.0D;
 		int temp = (int) (lastStoppedTime * 1000.0D);
 		lastStoppedTime = temp;
 		lastStoppedTime /= 1000.0D;
 	}
 
-	public double getlastStoppedTime() {
+	public static double getlastStoppedTime() {
 		return lastStoppedTime;
 	}
 
@@ -33,6 +34,13 @@ public class Main extends JavaPlugin {
 		Bukkit.getConsoleSender().sendMessage(
 				("&8[&cRBAntiChat&8] &aĐã khởi động thành công &f&o[&e&o" + getlastStoppedTime() + "s&f&o]")
 						.replace("&", "§"));
+		for (Player p : Bukkit.getOnlinePlayers()) {
+			if (p.hasPermission("rbantichat.admin")) {
+				p.sendMessage(("&8[&cRBAntiChat&8] &aĐã khởi động thành công &f&o[&e&o" + getlastStoppedTime() + "s&f&o]").replace("&", "§"));
+				return;
+			}
+		}
+		
 	}
 
 	public static Main getPlugin() {
